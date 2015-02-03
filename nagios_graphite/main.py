6aca4e00844c4f4e4fcab5adabd963217b25e0ae
Michael-Keith Bernard <mkbernard@opentable.com>
2015-02-03 14:55:53 -0800

    Refactor Graphite integration

--- nagios_graphite/main.py
+++ nagios_graphite/main.py
@@ -10,7 +10,10 @@ import urllib
 import requests
 from pynagios import Plugin, Response, make_option, UNKNOWN
 
+
 def percentile(n):
+    """Create function that calculates percentile for list"""
+
     return lambda xs: sorted(xs)[int(len(xs)*n)]
 
 FUNCTIONS = {
@@ -25,72 +28,86 @@ FUNCTIONS = {
 }
 F_OPTS = ", ".join(FUNCTIONS.keys())
 
+
 def combine(series, aggfn):
-    pairs = reduce(lambda a, b: a + b, [e["datapoints"] for e in series])
+    """Combine Graphite series data using aggfn"""
+
+    pairs = reduce(lambda a, b: a + b, [e["datapoints"] for e in series], [])
     points = [e[0] for e in pairs if e[0] is not None]
     return aggfn(points)
 
-class Graphite(object):
-    def __init__(self, options):
-        self.options = options
-        self._session = None
 
-        self.init_session()
-        self.aggfn = FUNCTIONS[options.func]
+def format_from(from_):
+    if not from_.startswith("-"):
+        return "-" + from_
+    else:
+        return from_
 
-    @property
-    def session(self):
-        if self._session is None:
-            self._session = requests.Session()
-        return self._session
 
-    @property
-    def query(self):
-        if not self.options.from_.startswith("-"):
-            from_ = "-" + self.options.from_
-        else:
-            from_ = self.options.from_
+def graphite_querystring(opts):
+    qs = {
+        "target": opts.target,
+        "from": format_from(opts.from_),
+        "format": "json",
+    }
 
-        qs = {
-            "target": self.options.target,
-            "from": from_,
-            "format": "json",
-        }
-        return urllib.urlencode(qs)
+    return urllib.urlencode(qs)
 
-    def init_session(self):
-        if self.options.username:
-            self.session.auth(
-                self.options.username,
-                self.options.password)
 
-    def fetch(self):
-        url = "{}?{}".format(self.options.hostname, self.query)
-        results = self.session.get(url)
-        if results.ok and results.json():
-            return combine(results.json(), self.aggfn)
-        return None
+def graphite_url(opts):
+    qs = graphite_querystring(opts)
+    return "{}?{}".format(opts.hostname, qs)
+
+
+def graphite_session(opts):
+    session = requests.Session()
+    if opts.username:
+        session.auth = (opts.username, opts.password)
+    return session
+
+
+def graphite_fetch(opts, session=None):
+    if session is None:
+        session = graphite_session(opts)
+
+    url = graphite_url(opts)
+    resp = session.get(url)
+
+    return resp.json() if resp.ok else []
+
+
+def check_graphite(opts, session=None):
+    aggfn = FUNCTIONS[opts.func]
+    raw_data = graphite_fetch(opts)
+
+    return combine(raw_data, aggfn)
 
 class GraphiteNagios(Plugin):
-    username = make_option("--username", "-U",
+    username = make_option(
+        "--username", "-U",
         help="Username (HTTP Basic Auth)")
-    password = make_option("--password", "-P",
+    password = make_option(
+        "--password", "-P",
         help="Password (HTTP Basic Auth)")
 
-    name = make_option("--name", "-N",
+    name = make_option(
+        "--name", "-N",
         help="Metric name", default="metric")
-    target = make_option("--target", "-M",
+    target = make_option(
+        "--target", "-M",
         help="Graphite target (series or query)")
-    from_ = make_option("--from", "-F",
+    from_ = make_option(
+        "--from", "-F",
         help="Starting offset", default="1minute")
 
-    func = make_option("--algorithm", "-A",
+    func = make_option(
+        "--algorithm", "-A",
         help=("Algorithm for combining metrics, options: "
               "{}, (default: avg)".format(F_OPTS)),
         default="avg", choices=FUNCTIONS.keys())
 
     def check(self):
-        value = Graphite(self.options).fetch()
+        value = check_graphite(self.options)
         if value is None:
             return Response(UNKNOWN, "No results returned!")
 
@@ -100,11 +117,14 @@ class GraphiteNagios(Plugin):
         response.set_perf_data(self.options.func, value)
         return response
 
+
 def main(args):
     try:
         return GraphiteNagios(args).check().exit()
     except Exception as e:
-        return Response(UNKNOWN, "Client error: " + str(e))
+        message = "{}: {}".format(e.__class__.__name__, str(e))
+        Response(UNKNOWN, "Client error: " + message).exit()
+
 
 def entry_point():
     return main(sys.argv)
