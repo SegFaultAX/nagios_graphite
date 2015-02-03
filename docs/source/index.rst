7d7d517682ed4c830e4b0a5406deb961001630e3
Michael-Keith Bernard <mkbernard@opentable.com>
2015-02-02 22:59:23 -0800

    Initial commit

---
+++ docs/source/index.rst
@@ -0,0 +1,17 @@
+Nagios Graphite
+===============
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+
+.. only:: html
+
+   Indices and tables
+   ==================
+
+   * :ref:`genindex`
+   * :ref:`modindex`
+   * :ref:`search`
---
+++ nagios_graphite/metadata.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""Project metadata
+
+Information describing the project.
+"""
+
+# The package name, which is also the "UNIX name" for the project.
+package = 'nagios_graphite'
+project = "Nagios Graphite"
+project_no_spaces = project.replace(' ', '')
+version = '0.1'
+description = 'Nagios command for Graphite metrics'
+authors = ['Michael-Keith Bernard']
+authors_string = ', '.join(authors)
+emails = ['mkbernard.dev@gmail.com']
+license = 'MIT'
+copyright = '2015 ' + authors_string
+url = 'https://github.com/segfaultax/nagios_graphite'
